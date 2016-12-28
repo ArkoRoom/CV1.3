2096ef04fa67fc2d4b0a12f01e8ca8fbb959609d
Arkolips <catel.ludovic@gmail.com>
2016-12-28 17:04:57 +0100

    setup the project from scratch

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ludovic CATEL</title>
+    <link rel="stylesheet" href="private/vendor/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="public/css/style.css" media="screen" title="no title">
+  </head>
+  <body>
+
+    <script src="private/vendor/jquery/dist/jquery.min.js" charset="utf-8"></script>
+    <script src="private/vendor/bootstrap/dist/js/bootstrap.min.js" charset="utf-8"></script>
+    <script src="public/js/script.js" charset="utf-8"></script>
+  </body>
+</html>
