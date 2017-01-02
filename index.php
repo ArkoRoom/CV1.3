8c87d304181d7b7f2824fa8770535919603c1186
Arkolips <catel.ludovic@gmail.com>
2017-01-02 18:09:06 +0100

    add the bower.json

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
     <link rel="stylesheet" href="public/css/style.css" media="screen" title="no title">
   </head>
   <body>
+    <div class="container">
+      <div class="row">
+        <h1>DEVELOPPEUR WEB JUNIOR</h1>
+        <h3>CATEL Ludovic</h3>
+        <p>27 ans, Lille - Nord</p>
+        <div class="col-md-6">
+          <i></i>
+        </div>
+        <div class="col-md-6">
+          <i></i>
+        </div>
+      </div>
+    </div>
+
 
     <script src="private/vendor/jquery/dist/jquery.min.js" charset="utf-8"></script>
     <script src="private/vendor/bootstrap/dist/js/bootstrap.min.js" charset="utf-8"></script>
