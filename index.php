47fa81855a710da5ca48703d737f78191402de72
Arkolips <catel.ludovic@gmail.com>
2017-01-02 18:31:06 +0100

    add the nav and the folder img

--- index.php
+++ index.php
@@ -3,23 +3,58 @@
   <head>
     <meta charset="utf-8">
     <title>Ludovic CATEL</title>
-    <link rel="stylesheet" href="private/vendor/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title">
-    <link rel="stylesheet" href="public/css/style.css" media="screen" title="no title">
+    <link rel="stylesheet" href="private/vendor/bootstrap/dist/css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="private/vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="public/css/style.css" media="screen">
   </head>
   <body>
     <div class="container">
-      <div class="row">
+      <!-- Header -->
+      <div class="row" id="header">
         <h1>DEVELOPPEUR WEB JUNIOR</h1>
         <h3>CATEL Ludovic</h3>
         <p>27 ans, Lille - Nord</p>
         <div class="col-md-6">
-          <i></i>
+          <i class="fa fa-envelope" aria-hidden="true"></i>catel.ludovic@gmail.com
         </div>
         <div class="col-md-6">
-          <i></i>
+          <i class="fa fa-phone"></i> 06 13 32 59 04
         </div>
       </div>
+      <!-- /Header -->
     </div>
+    <!-- /container -->
+    <!-- Nav -->
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <img alt="Brand" src="public/img/cherry.jpg" width="25">
+          </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- /Nav -->
+    <!-- Container -->
+    <div class="container">
+
+    </div>
+    <!-- /Container -->
 
 
     <script src="private/vendor/jquery/dist/jquery.min.js" charset="utf-8"></script>
