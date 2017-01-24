7cb8dc8db67e744fec0945579acde20add40898a
Arkolips <catel.ludovic@gmail.com>
2017-01-24 16:30:16 +0100

    finish to add the structure of the site

--- index.php
+++ index.php
@@ -8,25 +8,30 @@
     <link rel="stylesheet" href="public/css/style.css" media="screen">
   </head>
   <body>
+    <!-- Container -->
     <div class="container">
+
       <!-- Header -->
       <div class="row" id="header">
         <h1>DEVELOPPEUR WEB JUNIOR</h1>
         <div class="col-md-4 col-md-offset-2">
           <img src="" alt="">
-          <h3>CATEL Ludovic</h3>
+          <h4>CATEL Ludovic</h3>
         </div>
         <div class="col-md-4">
-          <p>
-            27 ans, Lille - Nord <br>
+          <h4>
+            27 ans <br>
+            Lille - Nord <br>
             <i class="fa fa-envelope" aria-hidden="true"></i> catel.ludovic@gmail.com <br>
             <i class="fa fa-phone"></i> 06 13 32 59 04
-          </p>
+          </h4>
         </div>
       </div>
       <!-- /Header -->
+
     </div>
     <!-- /container -->
+
     <!-- Nav -->
     <nav class="navbar navbar-default">
       <div class="container-fluid">
@@ -53,9 +58,260 @@
       </div>
     </nav>
     <!-- /Nav -->
+
     <!-- Container -->
     <div class="container">
+      <div class="row" id="section">
+        <section>
+          <h4>Bienvenue sur mon CV !</h4>
+          <p>
+            A l'origine formé dans le domaine de la Communication et des Industries Graphiques, c'est en 2015 que j'ai décidé de me réorienter dans le domaine du Web. <br>
+            Ne connaissant pas ce domaine, j'ai eu l'opportunité de participer à une initiation aux métiers du numériques au sein du programme Btech à la Cyber-Base d'Euratechnologies à Lille.<br>
+            A la suite de cette initiation, j'ai décidé de poursuivre ma formation en rejoignant la première session de Webforce3 à Lille.<br><br>
+            Vous pouvez accéder aux certificats obtenus lors de la formation Webforce3 en cliquant ci-dessous.
+          </p>
+          <div class="center-align">
+            <a class="btn" href="public/download/integrateur.pdf" target="_blank"><i class=""></i>Intégrateur Web</a>
+            <a class="btn" href="public/download/developpeur.pdf" target="_blank"><i class=""></i>Développeur Web</a>
+          </div>
+          <div class="col-md-8 col-md-offset-2" id="arrow">
+            <a href="#experiences" class="arrow-bottom">
+              <div>
+                <i class="fa fa-chevron-down fa-3x"></i>
+                <i class="fa fa-chevron-down fa-2x"></i>
+              </div>
+            </a>
+          </div>
+        </section>
+        <section>
+          <h4>Mes Expériences</h4>
+          <p>
+            Etant actuellement Développeur Junior, je n'ai pas d'expériences significatives dans le domaine du Web.
+            Je vais donc vous mettre les projets sur lesquels j'ai travaillé lors de mon initiation Btech et de ma formation Webforce3 à Lille.
+            <br><br>
+            <span>Aout 2016 :</span> Projet "Famil'Ease", site de gestion pour famille divorcé.
+            J'ai été en charge d'une grosse partie du développement frontend.
+            Je venais également en aide quand je le pouvais sur divers petits bugs.
+            <br><br>
+            <span>Février 2016 :</span> Refonte du site "x2000", association dans le domaine des formations numériques à Lille.
+            Cette refonte n'a pas été sélectionnée et étant réalisée lors d'une initiation aux métiers du numérique, il y a forcément des erreurs de développement (tel que les includes en PHP, les formulaires qui ne fonctionnent pas...).
+            <br><br>
+            <span>Septembre 2012 à Aout 2015 :</span> Diverses expériences dans le domaine des industries graphiques en intérim ou CDD.
+          </p>
+          <div class="col-md-8 col-md-offset-2" id="arrow">
+            <a href="#experiences" class="arrow-bottom">
+              <div>
+                <i class="fa fa-chevron-down fa-3x"></i>
+                <i class="fa fa-chevron-down fa-2x"></i>
+              </div>
+            </a>
+          </div>
+        </section>
+        <section>
+          <h4>Mes Compétences</h4>
+          <p>
+            Voici le détail de mes compétences :
+          </p>
+          <div class="col-md-5 col-md-offset-1">
+            <h5>Web</h5>
+            <table class="bordered highlight responsive-table">
+              <thead>
+                <tr>
+                  <th data-field="id">Langages</th>
+                  <th data-field="framework">Framework</th>
+                  <th data-field="tools">Outils</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>HTML5</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>CSS3</td>
+                  <td>Bootstrap, Materialize</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Javascript</td>
+                  <td>jQuery</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>PHP 5 & 7</td>
+                  <td>Architecture MVC</td>
+                  <td>POO</td>
+                </tr>
+                <tr>
+                  <td>SQL (MySQL)</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+            <p>
+              <span>Autres : </span>Sublime Text, Atom, Npm, Bower, Composer, Git, Trello.
+            </p>
+          </div>
+          <div class="col-md-5 col-md-offset-1">
+            <h5>Print</h5>
+            <table class="bordered highlight responsive-table">
+              <thead>
+                <tr>
+                  <th data-field="softwares">Logiciels</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Adobe InDesign</td>
+                </tr>
+                <tr>
+                  <td>Adobe Photoshop</td>
+                </tr>
+                <tr>
+                  <td>Adobe Illustrator</td>
+                </tr>
+              </tbody>
+            </table>
+            <p>
+              <span>Autres : </span>Créo, Preps7, Microsoft Office.
+            </p>
+          </div>
+          <div class="col-md-8 col-md-offset-2" id="arrow">
+            <a href="#experiences" class="arrow-bottom">
+              <div>
+                <i class="fa fa-chevron-down fa-3x"></i>
+                <i class="fa fa-chevron-down fa-2x"></i>
+              </div>
+            </a>
+          </div>
+        </section>
+        <section>
+          <h4>Mes Créations</h4>
+          <ul class="collapsible" data-collapsible="expandable">
+            <li>
+              <div class="collapsible-header active"><i class="material-icons">filter_drama</i>Réalisations / Participations</div>
+              <div class="collapsible-body">
+                <p>
+                  Vous pouvez trouver ici quelques liens vers les sites que j'ai pu réaliser lors de mes formations. <br>
+                  Vous avez aussi un lien vers mon profil LinkedIn ainsi que vers mon GitHub.
+                </p>
+                <ul class="center">
+                  <a href="http://btech-lille.com/btech7/x2000/html/formations.html" target="_blank">x2000</a>
+                  <a href="https://github.com/ArkoRoom" target="_blank">Github</a>
+                  <a href="https://fr.linkedin.com/in/ludovic-catel" target="_blank">LinkedIn</a>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <div class="collapsible-header"><i class="material-icons">place</i>Réalisation / Mini-jeux</div>
+              <div class="collapsible-body">
+                <div class="text-rules">
+                  <h3 class="center">Mini-Jeux du <br><i>C'est plus ou c'est moins !</i></h3>
+                  <p>
+                    Bienvenue dans ce Mini-Jeu ! <br>
+                    La règle est simple. L'ordinateur a sélectionner un chiffre aléatoirement entre 1 et 100. <br>
+                    Vous disposez de 6 éssais maximum pour trouver ce chiffre. <br>
+                    Bonne chance !
+                  </p>
+                </div>
+                <form id="formUser">
+                  <div class="form-group">
+                    <label for="answerUser">Votre réponse</label>
+                    <input type="number" name="answerUser" id="answerUser" value="">
+                  </div>
+                  <input class="btn btn-default" type="button" name="validAnswer" id="validAnswer" value="Valider">
+                </form>
+                <div class="answer">
 
+                </div>
+                  <input type="button" name="restart" class="btn" id="restart" value="Rejouez">
+              </div>
+            </li>
+            <li>
+              <div class="collapsible-header"><i class="material-icons">whatshot</i>Réalisation / Effet</div>
+              <div class="collapsible-body">
+                <div class="use-advice">
+                      <h3>Discover</h3>
+                      <p>
+                        Discover est un petit script qui permets de révéler une partie de l'image en cliquant simplement sur le bouton sous l'image.<br>
+                        Des carrés disparaissent alors permettant de révéler une partie de l'image.<br>
+                        L'image est générée grâce à une variable aléatoire.
+                      </p>
+                    </div>
+                    <div class="col-md-10 col-md-offset-1">
+                      <div class="wrap-img">
+                        <div class="container-div">
+
+                        </div>
+                      </div>
+                    </div>
+                    <input type="button" class="btn btn-default" id="delete-div" name="name" value="Supprimer des carrés">
+              </div>
+            </li>
+          </ul>
+          <div class="col-md-8 col-md-offset-2" id="arrow">
+            <a href="#experiences" class="arrow-bottom">
+              <div>
+                <i class="fa fa-chevron-down fa-3x"></i>
+                <i class="fa fa-chevron-down fa-2x"></i>
+              </div>
+            </a>
+          </div>
+        </section>
+        <section>
+          <h4>Me Contacter</h4>
+          <p>
+            Vous arrivez aux termes de ce CV.<br>
+            Merci de votre visite et n'hésitez pas à me contacter via le formulaire ci-dessous pour toutes demandes et/ou renseignements.<br>
+            N'hésitez pas à me suivre sur mes réseaux sociaux disponible dans le bas de la page.<br>
+            Vous pouvez également me contacter directement à cette adresse : <span>catel.ludovic@gmail.com.</span><br>
+            Ou via le formulaire ci-dessous. <br><br>
+            A bientôt !
+          </p>
+          <form class="col l4 offset-l4 s10 offset-s1" id="form-contact" action="index.php" method="post">
+                      <div class="input-field col s12">
+              <i class="material-icons prefix">account_circle</i>
+              <input id="icon_prefix" type="text" class="validate" name="name" placeholder="Votre nom...">
+                          <label for="icon_prefix">Nom</label>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">account_circle</i>
+              <input id="icon_prefix" type="text" class="validate" name="firstname" placeholder="Votre prénom...">
+                          <label for="icon_prefix">Prénom</label>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">email</i>
+              <input id="icon_telephone" type="tel" class="validate" name="mail" placeholder="Votre adresse e-mail...">
+                          <label for="icon_telephone">Adresse e-mail</label>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">textsms</i>
+              <input id="icon_prefix" type="text" class="validate" name="subject" placeholder="Sujet de votre message...">
+                          <label for="icon_prefix">Sujet</label>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">description</i>
+              <textarea id="textarea1" class="materialize-textarea" rows="8" name="message" placeholder="Votre message..."></textarea>
+                          <label for="textarea1">Message</label>
+            </div>
+            <div class="center-align">
+              <button class="btn waves-effect waves-light" type="submit" name="submit">Envoyer
+                <i class="material-icons right">done</i>
+              </button>
+            </div>
+          </form>
+          <div class="col-md-8 col-md-offset-2" id="arrow">
+            <a href="#experiences" class="arrow-bottom">
+              <div>
+                <i class="fa fa-chevron-down fa-3x"></i>
+                <i class="fa fa-chevron-down fa-2x"></i>
+              </div>
+            </a>
+          </div>
+        </section>
+      </div>
     </div>
     <!-- /Container -->
 
